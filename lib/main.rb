061c32cf2527c9ccfd10fd643f86da3e12b4228e
hishambokhari <hish.b92@gmail.com>
2020-07-25 16:58:08 +0100

    Main page demonstrating results

--- lib/main.rb
+++ lib/main.rb
@@ -1,6 +1,20 @@
 require_relative 'catalog'
 
-def results(result)
+def final_results(results)
   puts "Total Visits:"
-  results.each {|key, value| puts "#{key} and #{value} visits in total"}
+  results.each {|key, value| puts "#{key} had #{value} visits in total"}
 end
+
+def unique_results(results)
+  puts " "
+  puts " "
+  puts "Unique Visits:"
+  results.each {|key, value| puts "#{key} had #{value} unique visits"}
+end
+
+c = Catalog.new('webserver.log')
+page_visits = c.page_visits
+unique_visits = c.fill_hash
+
+final_results(page_visits)
+unique_results(unique_visits)
