6168907ad64f98d8aa6a0b2b4f86654aafe6b2a7
hishambokhari <hish.b92@gmail.com>
2020-07-25 11:37:40 +0100

    parse

--- lib/catalog.rb
+++ lib/catalog.rb
@@ -1,9 +1,14 @@
+require 'csv' # It's used for exporting & importing data
 
 class Catalog
   def initialize(file_path)
     @file_path = file_path #instance variable
   end
 
-  
+  def parse
+    pages_with_coordinates = []
+    CSV.foreach(@file_path){|row| pages_with_coordinates << row[0].split(' ')}
+    return pages_with_coordinates
+  end
 
 end
